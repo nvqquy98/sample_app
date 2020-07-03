cfbcb78f8bf01a38f82a17d9ff78a339cf010355
nvqquy98 <nvqquy98@gmail.com>
2020-07-03 11:34:09 +0700

    Complete Chapter 3 4 5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title page_title =""
+    base_title ="Rails Tutorial in 14days"
+    page_title.empty? ? base_title : "#{page_title} | #{base_title}"
+  end
 end

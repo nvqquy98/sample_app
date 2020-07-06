78d1fbbd7fa74e106fdff3feffe392dcb220dab3
nvqquy98 <nvqquy98@gmail.com>
2020-07-06 10:07:58 +0700

    Completed Chapter7 SignUp

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  root 'static_pages#home'
+  get '/help',  to: 'static_pages#help'
+  get '/signup' ,to:'users#new'
+  resources :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

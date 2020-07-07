9663a5b1dc9321399751477cc08a7c697403c27b
nvqquy98 <nvqquy98@gmail.com>
2020-07-07 10:09:06 +0700

    complete chapter9 Advanced-Login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,14 @@
 class SessionsController < ApplicationController
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
-    if user&.authenticate(params[:session][:password])
-      log_in user
-      redirect_to user
+    @user = User.find_by(email: params[:session][:email].downcase)
+    if @user&.authenticate(params[:session][:password])
+      log_in @user
+      params[:session][:remember_me] == "1" ? remember(@user) : forget(@user)
+      redirect_to @user
     else
       # Create an error message.
       flash.now[:danger] = " Email or Password incorrect !"
@@ -14,7 +17,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,11 +3,25 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-  def current_user
-    return unless session[:user_id]
+  def remember user
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
 
-    current_user = User.find_by id: session[:user_id]
-    return current_user if current_user
+  def forget user
+    user.forget
+    cookies.delete :user_id
+    cookies.delete :remember_token
+  end
+
+  def current_user
+    if (user_id = session[:user_id])
+      User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      log_in user if user&.authenticated?(cookies[:remember_token])
+    end
   end
 
   def logged_in?
@@ -15,6 +29,7 @@ module SessionsHelper
   end
 
   def log_out
+    forget current_user
     session.delete :user_id
   end
 end
